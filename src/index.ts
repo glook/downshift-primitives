e2c13db7768427050b56110d8fc069e1f9ab6e75
Andrey Polyakov <andrey@Polyakov.im>
2023-07-23 13:41:26 +0500

    setting package.json fixed lost DownshiftListboxContainer export

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 // Created by: Andrey Polyakov (andrey@polyakov.im)
-
+export {DownshiftArrow} from './downshift-arrow';
+export {DownshiftClear} from './downshift-clear';
+export {DownshiftCombobox} from './downshift-combobox';
+export {DownshiftInput} from './downshift-input';
 export {DownshiftLabel} from './downshift-label';
+export {DownshiftListboxContainer} from './downshift-listbox-container';
+export {DownshiftListBoxItems} from './downshift-listbox-items';
+export {DownshiftLoadingIndicator} from './downshift-loading-indicator';
 export {DownshiftOption} from './downshift-option';
 export {DownshiftOptionError} from './downshift-option-error';
-export {DownshiftOptionNoItems} from './downshift-option-no-items';
 export {
     DownshiftOptionLoading,
     DownshiftOptionLoadingMore,
 } from './downshift-option-loading';
-export {DownshiftArrow} from './downshift-arrow';
-export {DownshiftClear} from './downshift-clear';
-export {DownshiftCombobox} from './downshift-combobox';
-export {DownshiftTrigger} from './downshift-trigger';
-export {DownshiftInput} from './downshift-input';
-export {DownshiftLoadingIndicator} from './downshift-loading-indicator';
+export {DownshiftOptionNoItems} from './downshift-option-no-items';
 export {DownshiftSelect} from './downshift-select';
-export {DownshiftListBoxItems} from './downshift-listbox-items';
 export {DownshiftSelectPlaceholder} from './downshift-select-placeholder';
 export {DownshiftSelectedItem} from './downshift-selected-item';
+export {DownshiftTrigger} from './downshift-trigger';
 
 export type {
     DownshiftAsyncListLoadReturn,
