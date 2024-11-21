b900cb4dc9134f2b316ce83653cee02f768ec5cd
Andrey Polyakov <andrey@Polyakov.im>
2024-11-21 20:00:30 +0300

    Added an option Combobox.dropdownMenuFloatingOptions.applyWidth, Select.dropdownMenuFloatingOptions.applyWidth

--- src/hooks/useDropdownMenuFloating.ts
+++ src/hooks/useDropdownMenuFloating.ts
@@ -13,6 +13,7 @@ import {
 export interface DropdownMenuFloatingOptions
     extends Pick<UseFloatingOptions, 'placement'> {
     offset?: number;
+    applyWidth?: boolean;
 }
 
 export const useDropdownMenuFloating = (
@@ -21,20 +22,31 @@ export const useDropdownMenuFloating = (
 ) => {
     const placement = options?.placement ?? 'bottom-start';
     const offsetValue = options?.offset ?? 5;
+    const applyWidth = options?.applyWidth ?? true;
     return useFloating({
         middleware: [
             offset(offsetValue),
             flip(),
             shift(),
             size({
-                apply(options) {
-                    if (!options.elements.floating.style.minWidth) {
-                        const {reference} = options.elements;
-                        const {width} = reference.getBoundingClientRect();
-                        Object.assign(options.elements.floating.style, {
-                            minWidth: `${width}px`,
-                            maxWidth: `${width * 1.5}px`,
-                        });
+                apply: (options) => {
+                    const floatingRect = options.rects.floating;
+                    if (floatingRect.height !== 0) {
+                        const rectWidth = options.rects.reference.width;
+                        const floatingElement = options.elements.floating;
+                        floatingElement.style.setProperty(
+                            '--list-box-reference-width',
+                            `${rectWidth}px`,
+                        );
+                        floatingElement.style.setProperty(
+                            '--list-box-available-height',
+                            `${options.availableHeight}px`,
+                        );
+                        if(applyWidth) {
+                            floatingElement.style.width = `${rectWidth}px`;
+                            floatingElement.style.maxWidth = `${rectWidth * 1.5}px`;
+                        }
+
                     }
                 },
             }),
